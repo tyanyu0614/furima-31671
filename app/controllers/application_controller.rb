fec2cb0162df877a1123cedfa921e44a874a8d45
tyanyu0614 <73101963+tyanyu0614@users.noreply.github.com>
2020-11-15 14:19:53 +0900

    ビュー素材の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # before_action :basic_auth
 end
+
+# private
+
+# def basic_auth
+#   authenticate_or_request_with_http_basic do |username,password|
+#     username == 'admin' && password == '7777'
+#   end
+
