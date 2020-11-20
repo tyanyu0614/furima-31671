b2a3179e11c79bfabd3d25aa663e2377cbf50f1b
tyanyu0614 <73101963+tyanyu0614@users.noreply.github.com>
2020-11-20 10:58:12 +0900

    商品編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index,:show]
   before_action :baria_user, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:edit, :update, :show]
+
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -19,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -47,7 +47,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     unless current_user == @item.user
     redirect_to root_path 
+    end
   end
-end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
 
